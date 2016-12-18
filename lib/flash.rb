7e9b87c20da4171964538453570dea9802031ac4
Maddie Revill <maddierevill@gmail.com>
2016-12-18 11:13:35 -0800

    Implement flash cookie

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,19 +1,22 @@
 require 'json'
 
 class Flash
+  attr_reader :now
+
   def initialize(req)
     cookie = req.cookies["_rails_lite_app_flash"]
-    @flash = (cookie.nil? ? {} : JSON.parse(cookie))
-    @next_flash = {}
+    @now = (cookie.nil? ? {} : JSON.parse(cookie))
+    @flash = {}
   end
 
   def [](key)
-    @flash[key.to_s]
+    @flash[key.to_s] || @now[key.to_s]
   end
 
   def []=(key, val)
     @flash[key] = val
   end
+
 
   def store_flash(res)
     cookie_hash = { path: "/", value: @flash.to_json }
